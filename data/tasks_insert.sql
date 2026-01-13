9082f25842df538812377f1c9b4936004669d679
dhanushraja-m <2k21cse019@kiot.ac.in>
2026-01-13 15:22:20 +0530

    update structure

--- data/tasks_insert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tasks (id, task_name, parent_task_id) VALUES
-(1, 'Project Setup', NULL),
-(2, 'Requirement Analysis', 1),
-(3, 'Design', 1),
-(4, 'Backend Development', 3),
-(5, 'Frontend Development', 3),
-(6, 'Testing', 1),
-(7, 'Unit Testing', 6),
-(8, 'Integration Testing', 6);
